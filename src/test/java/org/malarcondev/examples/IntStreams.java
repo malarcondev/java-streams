2f4319a854be6ea14d1aa77717ee35a8a65ae6c4
Marcos Alarcon <proveedormarcos.alarcon@sura.pe>
2023-04-18 18:54:01 -0500

    add intStreams

---
+++ src/test/java/org/malarcondev/examples/IntStreams.java
@@ -0,0 +1,41 @@
+package org.malarcondev.examples;
+
+import org.junit.jupiter.api.Test;
+import org.malarcondev.beans.Person;
+import org.malarcondev.mock.MockData;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class IntStreams {
+
+    @Test
+    public void range() throws Exception {
+        System.out.println("with fori");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("with int stream exclusive");
+        IntStream.range(0, 10).forEach(System.out::println);
+
+        System.out.println("with int stream inclusive");
+        IntStream.rangeClosed(0, 10).forEach(System.out::println);
+    }
+
+    // Loop through people using IntStream
+    @Test
+    public void rangeIteratingLists() throws Exception {
+        List<Person> people = MockData.getPeople();
+        IntStream.range(0, people.size())
+                .forEach(index -> {
+                    System.out.println(people.get(index));
+                });
+    }
+
+    @Test
+    public void intStreamIterate()  {
+        IntStream.iterate(0, value -> value + 1)
+                .limit(11)
+                .forEach(System.out::println);
+    }
+}
